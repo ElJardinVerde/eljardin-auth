4a3ff03e056a62111ea266ef6517a23001bac478
DESKTOP-TCIVLML\iulian <iulian.pampu1984@gmail.com>
2024-06-27 08:09:53 +0300

    no message

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -113,7 +113,7 @@ export function Authentification() {
   };
 
   return (
-    <div className="min-h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+    <div className="min-h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
       <ThemeToggle />
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
       <Boxes />
